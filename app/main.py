a2a6d2a52df6c23cab78d683cee809ad79a47b0d
davidluz <davidmelo8@gmail.com>
2022-12-04 22:00:13 -0300

    Front e Back conectados

--- app/main.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, jsonify
-app = Flask(__name__)
-
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    return jsonify({'class_id': 'IMAGE_NET_XXX', 'class_name': 'Cat'})
-    
